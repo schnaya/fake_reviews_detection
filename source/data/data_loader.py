f19e9b0d7f0307ca04e05f725a925d3be7f13a1c
bbvuser <anastasia.schastnaya@bbv.by>
2025-06-26 16:52:57 +0300

    Finished exploration part

--- source/data/data_loader.py
+++ source/data/data_loader.py
@@ -11,7 +11,8 @@ class DataLoader:
     """
 
     def __init__(self):
-        self.__data_path = "../data"
+        self.__data_path = os.path.join("../", "data")
+        os.makedirs(self.__data_path, exist_ok=True)
 
     @property
     def data_path(self):
@@ -21,21 +22,50 @@ class DataLoader:
     def data_path(self, value):
         self.__data_path = value
 
-    def __download_kaggle_dataset(self, link):
+    @staticmethod
+    def __download_kaggle_dataset(link, res_dir):
         try:
             dataset_path = kagglehub.dataset_download(link, force_download=True)
-            os.makedirs(self.__data_path, exist_ok=True)
             for filename in os.listdir(dataset_path):
                 src = os.path.join(dataset_path, filename)
-                dst = os.path.join(self.__data_path, filename)
+                dst = os.path.join(res_dir, filename)
                 shutil.copy2(src, dst)
         except Exception as e:
             logging.error(e)
 
-    def __load_data(self, filename) -> pd.DataFrame:
-        return pd.read_csv(os.path.join(self.__data_path, filename))
+    @staticmethod
+    def __download_kaggle_file(link, filename, filepath):
+        try:
+            dataset_path = kagglehub.dataset_download(link, path=filename, force_download=True)
+            if not os.path.isfile(dataset_path):
+                logging.error(f"File '{filename}' not found in '{dataset_path}'."
+                              f"Directory files: {os.listdir(dataset_path)}")
+                return
+            shutil.copy2(dataset_path, filepath)
+        except Exception as e:
+            logging.error(e)
+
+    @staticmethod
+    def __load_data(filepath) -> pd.DataFrame:
+        return pd.read_csv(filepath, quotechar='"', encoding='utf-8', encoding_errors='replace')
+
+    def __prepare_paths_and_dirs(self, link, filename) -> tuple[str, str]:
+        res_dir = os.path.join(self.__data_path, *link.split('/'))
+        os.makedirs(res_dir, exist_ok=True)
+        filepath = os.path.join(res_dir, filename)
+        return res_dir, filepath
 
     def load_from_kaggle(self, link, filename) -> pd.DataFrame:
-        self.__download_kaggle_dataset(link)
-        return self.__load_data(filename)
+        res_dir, filepath = self.__prepare_paths_and_dirs(link, filename)
+        print(filepath)
+        if not os.path.isfile(filepath):
+            self.__download_kaggle_dataset(link, res_dir)
+        #    self.__download_kaggle_file(link, filename, filepath)
+        return self.__load_data(filepath)
 
+
+if __name__ == "__main__":
+    data_loader = DataLoader()
+    data_loader.data_path = "../../data"
+    mexwell_set = {"link": "mexwell/fake-reviews-dataset", "filename": "fake reviews dataset.csv"}
+    data_loader.load_from_kaggle(**mexwell_set)
--- source/data/data_preparer.py
+++ source/data/data_preparer.py
@@ -1,3 +1,10 @@
+from typing import Any, List, Optional, Union
+
+import pandas as pd
+
+from source.data import DataLoader
+
+
 class DataPreparer:
     """
     Class for cleaning, preprocessing, and preparing data for machine learning models.
@@ -6,11 +13,64 @@ class DataPreparer:
     splitting data, and handling class imbalance.
     Operates on DataFrames passed to its methods, returning transformed DataFrames/arrays.
     """
-    def __init__(self):
+    def __init__(self, df: pd.DataFrame):
+        self.__df = df.copy()
+
+    @property
+    def df(self):
+        return self.__df
+
+    def __drop_missing_values(self, cols_to_process: Optional[List[str]] = None) -> None:
+        self.__df.dropna(subset=cols_to_process, inplace=True)
+
+    def __fill_missing_values(self, cols_to_process: Optional[List[str]] = None, fill_value: Any = None) -> None:
+        self.__df[cols_to_process] = self.__df[cols_to_process].fillna(fill_value)
+
+    def handle_missing_values(self,
+                              strategy: str = 'drop',
+                              columns: Optional[List[str]] = None,
+                              fill_value: Any = None) -> None:
+
+        cols_to_process = columns if columns is not None else self.__df.columns[self.__df.isnull().any()].tolist()
+        if strategy == 'drop':
+            self.__drop_missing_values(cols_to_process)
+        elif strategy == 'fill':
+            if fill_value:
+                self.__fill_missing_values(cols_to_process, fill_value)
+            else:
+                raise KeyError(f'Missing value for {strategy}. Please provide a value for {cols_to_process}.')
+
+    def drop_duplicates(self, subset: Optional[List[str]] = None) -> None:
+        self.__df.drop_duplicates(subset=subset, keep='first', inplace=True)
+
+    def clean_text_column(self, df: pd.DataFrame, text_column: str, methods: Optional[List[str]] = None,
+                          stop_words_lang: Optional[str] = 'english', lemmatize: bool = False,
+                          stem: bool = False) -> None:
+        """
+        Cleans a specified text column using a list of methods.
+
+        Available methods: 'lower', 'remove_punctuation', 'remove_numbers',
+        'remove_whitespace', 'remove_stopwords'.
+
+        Requires nltk stopwords and wordnet (for lemmatization) if those methods are used.
+        """
+
         pass
 
-    def clean_data(self):
+    def create_text_features(self, df: pd.DataFrame, text_column: str, method: str = 'tfidf', **kwargs) -> Union[pd.DataFrame, pd.DataFrame]:
         pass
 
-    def clean_text(self):
-        pass
+    def prepare_df(self):
+        self.handle_missing_values()
+        self.drop_duplicates()
+        self.clean_data()
+
+
+if __name__ == '__main__':
+    loader = DataLoader()
+    loader.data_path = "../../data"
+    yelp_set = {"link": "thedevastator/yelp-reviews-sentiment-dataset", "filename": "train.csv"}
+    yelp = loader.load_from_kaggle(**yelp_set)
+    yelp_preparer = DataPreparer(yelp)
+    yelp_preparer.prepare_df()
+    res = yelp_preparer.df
